47c08dbf19e59463970d5d5ecdcdd07f17afd71b
venckusd@gmail.com <venckusd@gmail.com>
2024-03-08 04:32:15 +0200

    008

---
+++ 008/app.js
@@ -0,0 +1,18 @@
+console.log("Hello from far away!");
+
+
+let a = 10;
+let b = "10"
+let rez = a+a;
+
+let l1 = "b";
+let l2 = "a";
+
+console.log(l1+l2 + + l2 + l2);
+console.log(0.3 + 0.6);
+
+Boolean
+
+let t = true
+let f = false
+console.log(t || f);
