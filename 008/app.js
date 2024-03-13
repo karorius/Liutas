5c905ead953d3ba9422181c60d4a3802808bd4cc
venckusd@gmail.com <venckusd@gmail.com>
2024-03-13 07:46:52 +0200

    kovas

--- 008/app.js
+++ 008/app.js
@@ -1,18 +1,15 @@
-console.log("Hello from far away!");
+// Funkcija rand(), kuri grazina atsitiktini skaiciu nuo 0 iki max
+function rand(max) {
+    return Math.floor(Math.random() * (max + 1));
+}
 
+// Sukuriam tris kintamuosius ir jiems priskiriam atsitiktines reiksmes nuo 0 iki 25
+var skaicius1 = rand(25);
+var skaicius2 = rand(25);
+var skaicius3 = rand(25);
 
-let a = 10;
-let b = "10"
-let rez = a+a;
+// Randame vidurini skaiciu
+var vidurinisSkaicius = Math.min(Math.max(skaicius1, skaicius2), Math.max(Math.min(skaicius1, skaicius2), skaicius3));
 
-let l1 = "b";
-let l2 = "a";
-
-console.log(l1+l2 + + l2 + l2);
-console.log(0.3 + 0.6);
-
-Boolean
-
-let t = true
-let f = false
-console.log(t || f);
+// Spausdiname vidurini skaiciu su console.log()
+console.log("Vidurinis skaicius:", vidurinisSkaicius);
