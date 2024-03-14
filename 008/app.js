14b339ffaa3ce6cfa26455fe0dd2506172d241c5
venckusd@gmail.com <venckusd@gmail.com>
2024-03-14 08:18:43 +0200

    009

--- 008/app.js
+++ 008/app.js
@@ -1,15 +1,57 @@
-// Funkcija rand(), kuri grazina atsitiktini skaiciu nuo 0 iki max
-function rand(max) {
-    return Math.floor(Math.random() * (max + 1));
-}
+console.log("Hello from far away!");
+// var A = 5 ;
+// var B = 10 ;
+// var C = A +B + 22;
+// console.log(A,B,C);
+// console.log(A)
+// let x = 10;
+// let a = x + 5
+// console.log('a:', a);
+// kintamojo tipai
+let a = 10;
+let b = '10';
+let g;
+// let c = 'hello' negali sudeti skirtingu tipu
+// console.log(a+b)
+// console.log(a+a)
+let x ='an';
+let y ='tis';
+let c = 'hell';
+let rez = a+a;
+console.log(x+y);
+// skaicius rodomas juoda spalva o tekstas kita spalva
+console.log(rez);
+console.log(a + + b)
+// Jeigu pries teksta pridesime dar viena pliusa jis pavercia teksta skaiciumi ir sudeda
+let l1 ='B'
+let l2 = 'a'
+console.log(l1 + l2 + + l2 +l2)
+console.log(a + '' + a);
+// Jeigu pries skaiciu pridesime tuscias '' kabutes tai skaiciu paversime stringu
+console.log(typeof a)
+console.log(typeof b)
+console.log(typeof +c)
+console.log(a-b);
+// console.log(0.3 + 0.6)dvejataine sistema nera suderinama su desimtaine ir galime taip panaudoti
+console.log((0.3 + 0.6).toFixed(1));
+a=50
+// g = a;
+console.log(a,g)
+console.log(g, typeof g)
+// neapsisprendes
+console.log(g+a);
+console.log(b+g);
+// Boolean
+let t = true
+let f = false ;
 
-// Sukuriam tris kintamuosius ir jiems priskiriam atsitiktines reiksmes nuo 0 iki 25
-var skaicius1 = rand(25);
-var skaicius2 = rand(25);
-var skaicius3 = rand(25);
+console.log( t+f)
+console.log( t/f)
+console.log( t*f)
+// bet koks skaicius yra true
+console.log( !!b)
+console.log( !!a)
+// loginiai operatoriai
 
-// Randame vidurini skaiciu
-var vidurinisSkaicius = Math.min(Math.max(skaicius1, skaicius2), Math.max(Math.min(skaicius1, skaicius2), skaicius3));
-
-// Spausdiname vidurini skaiciu su console.log()
-console.log("Vidurinis skaicius:", vidurinisSkaicius);
+console.log( t && f)
+console.log( t || f)
---
+++ 008/app2.js
@@ -0,0 +1,66 @@
+console.log("uzdaviniai");
+
+// function rand(min, max) {
+//     min = Math.ceil(min);
+//     max = Math.floor(max);
+//     return Math.floor(Math.random() * (max - min + 1)) + min;
+// }
+// let one = rand(0,4)
+// let two = rand(0,4)
+// if (one => two) {
+//     console.log(one/two);
+// } else {
+//     console.log(two/one);
+// }
+// if(5>3) {
+// console.log('5 is greater than 3');
+// console.log('5 is greater than 3');}
+
+
+console.log(Math.ceil(45.1))
+console.clear();
+let str = 'barsukas'
+console.log(str.length);
+console.log(str[0]+str[1]);
+console.clear();
+
+console.log('aaa'>'aa');
+console.clear();
+let v;
+if(1) {
+    v= 'jo';
+} else {
+
+    v='ne';
+}
+
+console.log(v)
+
+
+
+
+
+
+let A = -88;
+let aRez;
+ 
+if (A > 0) {
+    aRez = 'Teigiamas';
+} else if (A < 0) {
+    aRez = 'Neigiamas';
+} else {
+    aRez = 'Nulis';
+}
+ 
+console.log(aRez);
+ 
+let aRez2 = (A > 0) ? 'Teigiamas' : (A < 0) ? 'Neigiamas' : 'Nulis';
+ 
+console.log(aRez2);
+
+let z1 = 5
+let z2 = 8
+let zasys = z1 + 'zasys su'+ z2 +'zasiais';
+console.log(zasys);
+
+
